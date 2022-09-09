7416342a3f54f75aa23ecbd1c0ca9b216ff937c7
Gardas Avinash <gavinash@rbbn.com>
2022-09-09 14:10:04 -0400

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'odm-backend',
+    appId: '1:174224341147:web:2de6d909d9ff5d6c2d2e3e',
+    storageBucket: 'odm-backend.appspot.com',
+    apiKey: 'AIzaSyAHHsXOYYjN3DInH-JdUot5tXQjCgRKHc0',
+    authDomain: 'odm-backend.firebaseapp.com',
+    messagingSenderId: '174224341147',
+  },
   production: false
 };
 
